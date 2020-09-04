7ae23c83d29c9fc294f7f0d50283ce786f8cc207
OliLobster <sgyr1993@163.com>
2020-09-03 23:27:24 -0700

    Update Article.java

--- TinNewsAndroid/app/src/main/java/com/laioffer/tinnews/model/Article.java
+++ TinNewsAndroid/app/src/main/java/com/laioffer/tinnews/model/Article.java
@@ -1,5 +1,7 @@
 package com.laioffer.tinnews.model;
 
+import java.util.Objects;
+
 public class Article {
     public String author;
     public String content;
@@ -7,6 +9,27 @@ public class Article {
     public String publishedAt;
     public String title;
     public String url;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Article article = (Article) o;
+        return favorite == article.favorite &&
+                Objects.equals(author, article.author) &&
+                Objects.equals(content, article.content) &&
+                Objects.equals(description, article.description) &&
+                Objects.equals(publishedAt, article.publishedAt) &&
+                Objects.equals(title, article.title) &&
+                Objects.equals(url, article.url) &&
+                Objects.equals(urlToImage, article.urlToImage);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, content, description, publishedAt, title, url, urlToImage, favorite);
+    }
+
     public String urlToImage;
     public boolean favorite;
 }
