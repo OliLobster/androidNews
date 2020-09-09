398caa63c6dd216ba3fa6890f99f7c274a03b3e1
OliLobster <sgyr1993@163.com>
2020-09-08 23:57:07 -0700

    Update TinNewsApplication.java

--- TinNewsAndroid/app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
+++ TinNewsAndroid/app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -3,5 +3,9 @@ package com.laioffer.tinnews;
 import android.app.Application;
 
 public class TinNewsApplication extends Application {
-    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // TODO: new code here.
+    }
 }
