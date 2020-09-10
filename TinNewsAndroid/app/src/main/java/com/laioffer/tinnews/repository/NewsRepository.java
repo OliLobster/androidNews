f2dbd0f2ff2df65c2fd55f035a752e2cbc15ecde
OliLobster <sgyr1993@163.com>
2020-09-09 23:43:21 -0700

    Update NewsRepository.java

--- TinNewsAndroid/app/src/main/java/com/laioffer/tinnews/repository/NewsRepository.java
+++ TinNewsAndroid/app/src/main/java/com/laioffer/tinnews/repository/NewsRepository.java
@@ -1,4 +1,11 @@
 package com.laioffer.tinnews.repository;
 
 public class NewsRepository {
+
+    private final NewsApi newsApi;
+
+    public NewsRepository(Context context) {
+        newsApi = RetrofitClient.newInstance(context).create(NewsApi.class);
+    }
 }
+
