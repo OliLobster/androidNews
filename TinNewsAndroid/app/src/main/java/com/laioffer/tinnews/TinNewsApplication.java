04fa5de80409bf5430b7ab509e4e53db61caea86
OliLobster <sgyr1993@163.com>
2020-09-09 23:39:40 -0700

    Add imports

--- TinNewsAndroid/app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
+++ TinNewsAndroid/app/src/main/java/com/laioffer/tinnews/TinNewsApplication.java
@@ -2,6 +2,9 @@ package com.laioffer.tinnews;
 
 import android.app.Application;
 
+import com.ashokvarma.gander.Gander;
+import com.ashokvarma.gander.imdb.GanderIMDB;
+
 public class TinNewsApplication extends Application {
     @Override
     public void onCreate() {
